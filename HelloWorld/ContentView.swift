9b5d60d5f265b09cecf6a131a8cc777b1f0e20d9
Merch Visoiu <mvisoiu@tucows.com>
2020-02-28 15:01:53 -0500

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Merch Visoiu on 2020-02-28.
+//  Copyright © 2020 Ting. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
